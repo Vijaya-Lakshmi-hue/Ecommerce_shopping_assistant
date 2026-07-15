5cd3be6d568f953bac54abca2ce1035b005e076f
Vijaya-Lakshmi-hue <259616046+Vijaya-Lakshmi-hue@users.noreply.github.com>
2026-07-15 10:35:28 +0530

    Update ecommerce.py

--- ecommerce.py
+++ ecommerce.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import gradio as gr
 import torch
 from transformers import ( AutoTokenizer,AutoModelForCausalLM,
     Trainer,TrainingArguments,DataCollatorForLanguageModeling
@@ -216,7 +217,6 @@ data = {
 }
 
 df = pd.DataFrame(data)
-
 print(df)
 from datasets import load_dataset
 df.to_json("train_data.json",orient="records",indent=4)
@@ -229,14 +229,6 @@ def formatting(data):
   }
 dataset=dataset["train"].map(formatting)
 print(dataset)
-tokenizer=AutoTokenizer.from_pretrained(model_name)
-tokenizer.pad_token=tokenizer.eos_token
-def tokenize(prompt):
-  result=tokenizer(prompt["text"],padding="max_length",truncation=True,max_length=128)
-  result["labels"]=result["input_ids"].copy()
-  return result
-dataset=dataset.map(tokenize)
-print(dataset)
 model_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 model = AutoModelForCausalLM.from_pretrained(
     model_name
@@ -247,6 +239,15 @@ config=LoraConfig(r=8,lora_alpha=16,
                   lora_dropout=0.1,bias="none",task_type="CAUSAL_LM")
 model=get_peft_model(model,config)
 model.print_trainable_parameters()
+tokenizer=AutoTokenizer.from_pretrained(model_name)
+tokenizer.pad_token=tokenizer.eos_token
+def tokenize(prompt):
+  result=tokenizer(prompt["text"],padding="max_length",truncation=True,max_length=128)
+  result["labels"]=result["input_ids"].copy()
+  return result
+dataset=dataset.map(tokenize)
+print(dataset)
+
 data_collator=DataCollatorForLanguageModeling(
     tokenizer=tokenizer,mlm=False
 )
@@ -268,14 +269,45 @@ trainer=Trainer(
 trainer.train()
 model.save_pretrained("tinyllama_adapter")
 tokenizer.save_pretrained("tinyllama_adapter")
-device=next(model.parameters()).device
-prompt="""###Question:
-How do I update my profile?
+device = next(model.parameters()).device
 
-###Answer:
+def chatbot(question):
+
+    prompt = f"""### Question:
+{question}
+
+### Answer:
 """
-inputs=tokenizer(prompt,return_tensors="pt")
-inputs={k:v.to(device) for k,v in inputs.items()}
-with torch.no_grad():
-  output=model.generate(**inputs,max_new_tokens=100)
-print(tokenizer.decode(output[0],skip_special_tokens=True))
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+
+    with torch.no_grad():
+        output = model.generate(
+            **inputs,
+            max_new_tokens=100
+        )
+
+    answer = tokenizer.decode(
+        output[0],
+        skip_special_tokens=True
+    )
+
+    if "### Answer:" in answer:
+        answer = answer.split("### Answer:")[-1].strip()
+
+    return answer
+
+
+demo = gr.Interface(
+    fn=chatbot,
+    inputs=gr.Textbox(
+        label="Ask a Question"
+    ),
+    outputs=gr.Textbox(
+        label="Answer"
+    ),
+    title="E-Commerce AI Assistant"
+)
+
+demo.launch()
